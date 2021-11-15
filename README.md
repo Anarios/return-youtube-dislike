f547adf87f513c2b9bbf0a3b69ca80891bcd4e4b
Dmitrii Selivanov <selivano.d@gmail.com>
2021-11-15 03:39:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# youtube-dislike-chrome-extension
---
+++ popup.js
@@ -0,0 +1,10 @@
+/* popup-script.js */
+document.querySelector('#login')
+.addEventListener('click', function () {
+  chrome.runtime.sendMessage({ message: 'get_auth_token' });
+});
+
+document.querySelector('#log_off')
+.addEventListener('click', function () {
+  chrome.runtime.sendMessage({ message: 'log_off' });
+});
