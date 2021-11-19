c8d759627bdb1796b46ff3aae0449db538ad6acb
Dmitrii Selivanov <selivano.d@gmail.com>
2021-11-19 16:58:28 +0300

    Formatting

--- Extensions/chrome/background.js
+++ Extensions/chrome/background.js
@@ -9,51 +9,50 @@ const apiUrl = "https://return-youtube-dislike-api.azurewebsites.net";
 
 chrome.runtime.onMessageExternal.addListener(
   (request, sender, sendResponse) => {
-      if (request.message === "get_auth_token") {
-        // chrome.identity.getAuthToken({ interactive: true }, function (token) {
-        //   console.log(token);
-        //   chrome.identity.getProfileUserInfo(function (userInfo) {
-        //     console.log(JSON.stringify(userInfo));
-        //   });
-        // });
-      } else if (request.message === "log_off") {
-        // console.log("logging off");
-        // chrome.identity.clearAllCachedAuthTokens(() => console.log("logged off"));
-      } else if (request.message == "set_state") {
-        // console.log(request);
-        // chrome.identity.getAuthToken({ interactive: true }, function (token) {
-        let token = "";
-        fetch(`${apiUrl}/votes?videoId=${request.videoId}`, {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            Authorization: "Bearer " + token,
-          },
+    if (request.message === "get_auth_token") {
+      // chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      //   console.log(token);
+      //   chrome.identity.getProfileUserInfo(function (userInfo) {
+      //     console.log(JSON.stringify(userInfo));
+      //   });
+      // });
+    } else if (request.message === "log_off") {
+      // console.log("logging off");
+      // chrome.identity.clearAllCachedAuthTokens(() => console.log("logged off"));
+    } else if (request.message == "set_state") {
+      // console.log(request);
+      // chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      let token = "";
+      fetch(`${apiUrl}/votes?videoId=${request.videoId}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          sendResponse(response);
         })
-          .then((response) => response.json())
-          .then((response) => {
-            sendResponse(response);
-          })
-          .catch();
-        //});
-        return true;
-      } else if (request.message == "send_links") {
-        toSend = toSend.concat(request.videoIds.filter((x) => !sentIds.has(x)));
-        if (toSend.length >= 20) {
-          fetch(`${apiUrl}/votes`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(toSend),
-          });
-          for (const toSendUrl of toSend) {
-            sentIds.add(toSendUrl);
-          }
-          toSend = [];
+        .catch();
+      //});
+      return true;
+    } else if (request.message == "send_links") {
+      toSend = toSend.concat(request.videoIds.filter((x) => !sentIds.has(x)));
+      if (toSend.length >= 20) {
+        fetch(`${apiUrl}/votes`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(toSend),
+        });
+        for (const toSendUrl of toSend) {
+          sentIds.add(toSendUrl);
         }
+        toSend = [];
       }
-
+    }
   }
 );
 
--- Extensions/chrome/content-script.js
+++ Extensions/chrome/content-script.js
@@ -1,10 +1,10 @@
 chrome.runtime.sendMessage({}, (securityToken) => {
-	var script = document.createElement('script');
-	script.setAttribute("security-token", securityToken);
-	script.setAttribute("extension-id", chrome.runtime.id);
-	script.src = chrome.runtime.getURL('script.js');
-	script.onload = function() {
-		this.remove();
-	};
-	(document.head || document.documentElement).appendChild(script);
+  var script = document.createElement("script");
+  script.setAttribute("security-token", securityToken);
+  script.setAttribute("extension-id", chrome.runtime.id);
+  script.src = chrome.runtime.getURL("script.js");
+  script.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(script);
 });
