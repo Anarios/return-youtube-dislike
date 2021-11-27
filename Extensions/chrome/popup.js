0d0d46e59895d84998d42a88063c359db8a54242
Front <27463495+Frontesque@users.noreply.github.com>
2021-11-27 13:13:51 -0500

    :red_circle: Chrome Extension Popup Redesign

--- Extensions/chrome/popup.js
+++ Extensions/chrome/popup.js
@@ -1,4 +1,28 @@
-/* popup-script.js */
+/*   Config   */
+const config = {
+  links: {
+    website: 'https://returnyoutubedislike.com',
+    github: 'https://github.com/Anarios/return-youtube-dislike',
+    discord: 'https://discord.gg/mYnESY4Md5'
+  }
+};
+
+/*   Links   */
+document.getElementById('link_website').addEventListener('click', () => {
+  chrome.tabs.create({url: config.links.website});
+});
+
+document.getElementById('link_github').addEventListener('click', () => {
+  chrome.tabs.create({url: config.links.github});
+});
+
+document.getElementById('link_discord').addEventListener('click', () => {
+  chrome.tabs.create({url: config.links.discord});
+});
+
+
+
+/* popup-script.js 
 document.querySelector('#login')
 .addEventListener('click', function () {
   chrome.runtime.sendMessage({ message: 'get_auth_token' });
@@ -7,3 +31,4 @@ document.querySelector('#login')
 document.querySelector("#log_off").addEventListener("click", function () {
   chrome.runtime.sendMessage({ message: "log_off" });
 });
+*/
