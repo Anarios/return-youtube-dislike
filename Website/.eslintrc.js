2a5173c5d9b79eb68065bcddaed59884501e8d8f
Franco <francosanllehi@gmail.com>
2021-11-27 13:50:24 -0300

    feat(eslint): add eslint package and configs

---
+++ Website/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended',
+        'prettier'
+    ],
+    plugins: [
+    ],
+    // add your custom rules here
+    rules: {}
+}
