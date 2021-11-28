36dffffff92e4217bb0fa14e64fdd348e6ba4c7d
Front <27463495+Frontesque@users.noreply.github.com>
2021-11-28 17:43:44 -0500

    working on extension ui

---
+++ temp/popup.js
@@ -0,0 +1,59 @@
+/*   Config   */
+const config = {
+    advanced: false,
+    showAdvancedMessage: "Show Advanced",
+    hideAdvancedMessage: "Hide Advanced",
+
+    links: {
+        website: 'https://returnyoutubedislike.com',
+        github: 'https://github.com/Anarios/return-youtube-dislike',
+        discord: 'https://discord.gg/mYnESY4Md5'
+    },
+};
+
+/*   Links   */
+document.getElementById('link_website').addEventListener('click', () => {
+    chrome.tabs.create({
+        url: config.links.website
+    });
+});
+
+document.getElementById('link_github').addEventListener('click', () => {
+    chrome.tabs.create({
+        url: config.links.github
+    });
+});
+
+document.getElementById('link_discord').addEventListener('click', () => {
+    chrome.tabs.create({
+        url: config.links.discord
+    });
+});
+
+/*   Advanced Toggle   */
+const advancedToggle = document.getElementById('advancedToggle');
+advancedToggle.addEventListener('click', () => {
+    const adv = document.getElementById('advancedSettings');
+    if (config.advanced) {
+        adv.style.display = "none";
+        advancedToggle.innerHTML = config.showAdvancedMessage;
+        config.advanced = false;
+    } else {
+        adv.style.display = "block";
+        advancedToggle.innerHTML = config.hideAdvancedMessage;
+        config.advanced = true;
+    }
+});
+
+
+
+/* popup-script.js 
+document.querySelector('#login')
+.addEventListener('click', function () {
+  chrome.runtime.sendMessage({ message: 'get_auth_token' });
+});
+
+document.querySelector("#log_off").addEventListener("click", function () {
+  chrome.runtime.sendMessage({ message: "log_off" });
+});
+*/
