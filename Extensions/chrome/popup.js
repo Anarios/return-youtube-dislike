1fc81b445fd1571f12118ec79ef1245586df11b9
Nikita Krupin <krupin.nikita0110@gmail.com>
2021-12-01 17:45:24 -0500

    settings label

--- Extensions/chrome/popup.js
+++ Extensions/chrome/popup.js
@@ -5,29 +5,29 @@ const config = {
   hideAdvancedMessage: "Hide Settings",
 
   links: {
-    website: 'https://returnyoutubedislike.com',
-    github: 'https://github.com/Anarios/return-youtube-dislike',
-    discord: 'https://discord.gg/mYnESY4Md5'
+    website: "https://returnyoutubedislike.com",
+    github: "https://github.com/Anarios/return-youtube-dislike",
+    discord: "https://discord.gg/mYnESY4Md5",
   },
 };
 
 /*   Links   */
-document.getElementById('link_website').addEventListener('click', () => {
-  chrome.tabs.create({url: config.links.website});
+document.getElementById("link_website").addEventListener("click", () => {
+  chrome.tabs.create({ url: config.links.website });
 });
 
-document.getElementById('link_github').addEventListener('click', () => {
-  chrome.tabs.create({url: config.links.github});
+document.getElementById("link_github").addEventListener("click", () => {
+  chrome.tabs.create({ url: config.links.github });
 });
 
-document.getElementById('link_discord').addEventListener('click', () => {
-  chrome.tabs.create({url: config.links.discord});
+document.getElementById("link_discord").addEventListener("click", () => {
+  chrome.tabs.create({ url: config.links.discord });
 });
 
 /*   Advanced Toggle   */
-const advancedToggle = document.getElementById('advancedToggle');
-advancedToggle.addEventListener('click', () => {
-  const adv = document.getElementById('advancedSettings');
+const advancedToggle = document.getElementById("advancedToggle");
+advancedToggle.addEventListener("click", () => {
+  const adv = document.getElementById("advancedSettings");
   if (config.advanced) {
     adv.style.display = "none";
     advancedToggle.innerHTML = config.showAdvancedMessage;
@@ -38,8 +38,6 @@ advancedToggle.addEventListener('click', () => {
     config.advanced = true;
   }
 });
-
-
 
 /* popup-script.js 
 document.querySelector('#login')
