6aeb60d3e85ab4da085501ed822d565427ab9733
Front <27463495+Frontesque@users.noreply.github.com>
2021-12-02 21:41:33 -0500

    highlight button in extension, remove default_legacy, remove bloat from yoomoney page

--- Website/pages/pay/yoomoney.vue
+++ Website/pages/pay/yoomoney.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    
-    <section style="position: relative;">
-      <div class="center">
-        <center>
-
-          <iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&amp;targets=For%20returnyoutubedislike.com&amp;targets-hint=&amp;default-sum=&amp;button-text=13&amp;hint=&amp;successURL=&amp;quickpay=shop&amp;account=410015901550027&amp;" allowtransparency="true" scrolling="no" width="423" height="222" frameborder="0" />
-
-        </center>
-      </div>
-    </section>
-
+    <iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&amp;targets=For%20returnyoutubedislike.com&amp;targets-hint=&amp;default-sum=&amp;button-text=13&amp;hint=&amp;successURL=&amp;quickpay=shop&amp;account=410015901550027&amp;" allowtransparency="true" scrolling="no" width="423" height="222" frameborder="0" />
   </div>
 </template>
