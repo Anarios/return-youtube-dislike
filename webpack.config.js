d13aa3f0511659cff9278431758958ba9e20bd71
Dmitrii Selivanov <selivano.d@gmail.com>
2021-12-19 03:12:11 +0300

    Webpack fixes, build now builds both FF and chrome versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,57 @@
-const path = require("path");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "./Extensions/combined/ryd.content-script.js"),
+  entry: path.join(__dirname, './Extensions/combined/ryd.content-script.js'),
   output: {
-    filename: "bundled-content-script.js",
-    path: path.resolve(__dirname, "Extensions/combined"),
+    filename: 'bundled-content-script.js',
+    path: path.resolve(__dirname, 'Extensions/combined')
   },
-  externals: {
-    ramda: "R",
+  optimization: {
+    minimize: false
   },
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    // exclude locale files in moment
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './Extensions/combined',
+          to: './dist/chrome',
+          globOptions: {
+            ignore: ['**/manifest-**', '**/dist/**', '**/src/**','**/ryd.content-script.js']
+          }
+        },
+        {
+          from: './Extensions/combined/manifest-chrome.json',
+          to: './dist/chrome/manifest.json'
+        },
+        {
+          from: './Extensions/combined',
+          to: './dist/firefox',
+          globOptions: {
+            ignore: ['**/manifest-**', '**/dist/**', '**/src/**','**/ryd.content-script.js']
+          }
+        },
+        {
+          from: './Extensions/combined/manifest-firefox.json',
+          to: './dist/firefox/manifest.json'
+        }
+      ]
+    })
+  ]
 };
