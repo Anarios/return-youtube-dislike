a57cf4eb6de0300ab896e675ceb73620a8be0fde
Front <27463495+Frontesque@users.noreply.github.com>
2021-12-27 13:50:31 -0500

    clean up code

--- Website/pages/documentation.vue
+++ Website/pages/documentation.vue
@@ -4,14 +4,18 @@
       <v-card-title style="padding-bottom: 0;">Sections</v-card-title>
       <v-list>
         <v-list-item v-for="(item, i) in links" :key="i" router :to=item.to>
-            <v-list-item-icon><v-icon v-text="item.icon" /></v-list-item-icon>
-            <v-list-item-title style="text-align: left;"><v-list-item-title v-text="item.text" /></v-list-item-title>
+          <v-list-item-icon>
+            <v-icon v-text="item.icon" />
+          </v-list-item-icon>
+          <v-list-item-title style="text-align: left;">
+            <v-list-item-title v-text="item.text" />
+          </v-list-item-title>
         </v-list-item>
       </v-list>
     </v-card>
 
     <v-card max-width="600px" class="rounded-lg" style="margin: 1em; padding: 0.75em; text-align: left;">
-        <NuxtChild />
+      <NuxtChild />
     </v-card>
 
 
@@ -19,16 +23,32 @@
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {
-            links: [
-                { text: 'API Usage Rights', icon: 'mdi-book-open-variant', to: 'documenation/usage-rights' },
-                { text: 'API URL Information', icon: 'mdi-web', to: 'documenation/url' },
-                { text: 'Available Endpoints', icon: 'mdi-transit-connection-variant', to: 'documenation/endpoints' },
-                { text: 'Basic Fetching Tutorial', icon: 'mdi-school', to: 'documenation/fetching' },
-            ]
-        }
+      return {
+        links: [{
+            text: 'API Usage Rights',
+            icon: 'mdi-book-open-variant',
+            to: 'documenation/usage-rights'
+          },
+          {
+            text: 'API URL Information',
+            icon: 'mdi-web',
+            to: 'documenation/url'
+          },
+          {
+            text: 'Available Endpoints',
+            icon: 'mdi-transit-connection-variant',
+            to: 'documenation/endpoints'
+          },
+          {
+            text: 'Basic Fetching Tutorial',
+            icon: 'mdi-school',
+            to: 'documenation/fetching'
+          },
+        ]
+      }
     }
-}
+  }
+
 </script>
