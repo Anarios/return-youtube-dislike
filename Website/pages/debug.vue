45f7e811e9dfc8c874de4815088e18ed36a8cf0c
Front <27463495+Frontesque@users.noreply.github.com>
2021-12-27 18:45:11 -0500

    copy button

--- Website/pages/debug.vue
+++ Website/pages/debug.vue
@@ -4,7 +4,7 @@
     <v-card max-width="600px" class="rounded-lg">
       <v-card-title style="padding-bottom: 0">Debug Information</v-card-title>
       <v-card-text style="text-align: left;">
-        <!--   Dynamically Generate Links From Below   -->
+        <!--   Gather Browser Information   -->
         <span><b>Browser Information:</b></span><br>
         <span>Browser: {{ device._parsed.name }}</span><br>
         <span>Browser Vendor: {{ device._parsed.vendor }}</span><br>
@@ -13,6 +13,10 @@
         <span>Operating System Version: {{ device._parsed.os_version }}</span><br>
         <span>Device Type: {{ device._parsed.category }}</span><br>
       </v-card-text>
+      <v-card-actions>
+          <v-spacer />
+          <v-btn><v-icon small style="margin-right: 0.25em;">mdi-content-copy</v-icon>Copy</v-btn>
+      </v-card-actions>
     </v-card>
   </div>
 </template>
