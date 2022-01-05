22a6b0c53797375feda561f6fcbd17485bc59185
Nikita Krupin <krupin.nikita0110@gmail.com>
2022-01-04 19:24:58 -0500

    made me name bigger because I'm very important 😎

--- Website/pages/links.vue
+++ Website/pages/links.vue
@@ -28,11 +28,8 @@
     <p id="credits" class="flex-row no-wrap">
       Site by <v-icon color="#555">mdi-discord</v-icon> Front#2990
       <br />
-      <span style="color: #444; font-size: 0.75rem"
-        >&
-        <v-icon color="#333" size="1rem">mdi-discord</v-icon>
-        PickleNik#0864</span
-      >
+      & <v-icon color="#555">mdi-discord</v-icon>
+      PickleNik#0864
     </p>
   </div>
 </template>
