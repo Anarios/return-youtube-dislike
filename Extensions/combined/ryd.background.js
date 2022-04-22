4025503b9c5a9d03b4d575762e3eda0b3f6ce3ec
sy-b <94835959+sy-b@users.noreply.github.com>
2022-04-22 10:19:37 +0530

    Update ryd.background.js
    1. Race condition between two async calls
    2. We don't need to rewrite the variable every time a page is open.
    
     https://github.com/Anarios/return-youtube-dislike/pull/537#discussion_r855432386

--- Extensions/combined/ryd.background.js
+++ Extensions/combined/ryd.background.js
@@ -74,12 +74,12 @@ api.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
-api.storage.sync.get(['newInstallation'], (result) => {
-  if (result.newInstallation !== false) {
+api.storage.sync.get(['showChangelog'], (details) => {
+  if (details.showChangelog !== false) {
     api.tabs.create({url: api.runtime.getURL("/changelog/3/changelog_3.0.html")});
+    api.storage.sync.set({'showChangelog': false});
   }
 });
-api.storage.sync.set({'newInstallation': false});
 
 async function sendVote(videoId, vote) {
   api.storage.sync.get(null, async (storageResult) => {
